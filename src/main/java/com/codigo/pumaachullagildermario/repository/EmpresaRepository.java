dd8fdf11f30f73420ec5f7a3bf17e3465ed4c5ef
Mario <119698423+MarioGilder@users.noreply.github.com>
2024-04-20 13:05:30 -0500

    Update EmpresaRepository.java

--- src/main/java/com/codigo/pumaachullagildermario/repository/EmpresaRepository.java
+++ src/main/java/com/codigo/pumaachullagildermario/repository/EmpresaRepository.java
@@ -1,7 +1,5 @@
 package com.codigo.pumaachullagildermario.repository;
-
 import com.codigo.pumaachullagildermario.entity.EmpresaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface EmpresaRepository extends JpaRepository<EmpresaEntity,Long> {
 }
